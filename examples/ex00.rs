cd0e26d427f8785e2f704566e14e6b00b893b0ec
Michael Angerman <1809991+stormasm@users.noreply.github.com>
2024-06-17 08:22:03 -0700

    add an example directory along with removing the resolver and typechecker steps

---
+++ examples/ex00.rs
@@ -0,0 +1,50 @@
+// remove the resolver and typechecker from the main function
+
+use std::process::exit;
+
+use new_nu_parser::compiler::Compiler;
+use new_nu_parser::parser::Parser;
+//use new_nu_parser::resolver::Resolver;
+//use new_nu_parser::typechecker::Typechecker;
+
+fn main() {
+    let mut compiler = Compiler::new();
+
+    for fname in std::env::args().skip(1) {
+        let contents = std::fs::read(&fname);
+
+        let Ok(contents) = contents else {
+            eprintln!("can't find {}", fname);
+            exit(1);
+        };
+
+        let span_offset = compiler.span_offset();
+        compiler.add_file(&fname, &contents);
+
+        let parser = Parser::new(compiler, span_offset);
+        compiler = parser.parse();
+
+        compiler.print();
+
+        if !compiler.errors.is_empty() {
+            exit(1);
+        }
+        /*
+        let mut resolver = Resolver::new(&compiler);
+        resolver.resolve();
+        resolver.print();
+
+        compiler.merge_name_bindings(resolver.to_name_bindings());
+
+        if !compiler.errors.is_empty() {
+            exit(1);
+        }
+
+        let mut typechecker = Typechecker::new(&compiler);
+        typechecker.typecheck();
+        typechecker.print();
+
+        compiler.merge_types(typechecker.to_types());
+        */
+    }
+}
